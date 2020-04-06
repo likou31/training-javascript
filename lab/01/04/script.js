d2d3360eee41c1def085299a66ac518dbf1dcad7
Matt <amsellem.matt@gmail.com>
2020-04-06 17:50:15 +0200

    Finit Exo 01/04 et 02/01

---
+++ lab/01/04/script.js
@@ -0,0 +1,17 @@
+let myName = 'Amsellem';
+let resultatSomme = 10 + 10.25;
+function displayBooleanTrue() {
+    console.log(true);
+}
+
+function displayName() {
+    console.log(myName);
+}
+
+function displayNameUppercase() {
+    console.log(myName.toUpperCase());
+}
+
+function displaySum() {
+    console.log(resultatSomme);
+}
---
+++ lab/02/01/script.js
@@ -0,0 +1,3 @@
+function isModuloZero(a, b){
+    return (a % b == 0)?true:false;
+}
